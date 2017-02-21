a9a473a4c108455b66252156ba941aa8dbe07938
Varun Suresh <varunsureshkumar@g.harvard.edu>
2017-02-21 20:51:48 +0530

    php code added

--- index.php
+++ index.php
@@ -1,49 +1,79 @@
+<?php require('calculate.php'); ?>
+
 <!DOCTYPE html>
 
 <html>
+	  <head>
+		    <title>BMI Calculator</title>
+		    <meta charset='utf-8'>
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+		    <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
+		    <link href='styles.css' rel='stylesheet'>
+	  </head>
 
-<head>
-	<title>BMI Calculator</title>
-	<meta charset='utf-8'>
+	  <body>
+  	    <div class='container'>
+			      <h1>BMI Calculator</h1>
+			      <img src="bmi.jpg" alt="BMI Image"/>
 
-	<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
-	<link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
-	<link href='styles.css' rel='stylesheet'>
-</head>
+			      <form method='GET' action='index.php'>
+				    <fieldset>
+  				      <div class="form-group">
+  					       <label for='weight'>* Weight(lbs)</label>
+						       <input type='text' name='weight'  placeholder='Weight' autocomplete='off' class="form-control" value='<?php if(isset($_GET['weight'])) echo $_GET['weight'] ?>'><br>
+					      </div>
 
-<body>
-  <div class='container'>
-	<h1>BMI Calculator</h1>
-	<form method='POST' action='inputs.php'>
+  				      <div class="form-group">
+						        <label for='height'>* Height(inches)</label>
+						        <input type='text' name='height' placeholder='Height' autocomplete='off' class="form-control" value='<?php if(isset($_GET['height'])) echo $_GET['height'] ?>'><br>
+  				      </div>
 
-  <fieldset>
-  <div class="form-group">
-  <label for='weight'>* Weight</label>
-	<input type='text' name='weight'  placeholder='Weight' autocomplete='off' class="form-control"><br>
-	</div>
+					      <div class="form-group">
+						        <label for='age'>* Age</label>
+						        <input type='text' name='age' placeholder='Age' autocomplete='off' class="form-control" value='<?php if(isset($_GET['age'])) echo $_GET['age'] ?>'><br>
+					      </div>
 
-  <div class="form-group">
-	<label for='height'>* Height</label>
-	<input type='text' name='height' placeholder='Height' autocomplete='off' class="form-control"><br>
-  </div>
+					      <div class="form-group">
+						         <label>Gender:</label>
+  					         <label ><input type='radio' name='gender' value='male' checked='checked' <?php if($gender == 'male') echo 'CHECKED'?>>Male</label>
+  					         <label ><input type='radio' name='gender' value='female' <?php if($gender == 'female') echo 'CHECKED'?> >Female</label>
+  				      </div>
 
-  <div class="form-group">
-	<label>Activity level:</label>
-  <label ><input type='radio' name='day' value='sedentary' checked='checked'> Sedentary</label>
-  <label ><input type='radio' name='day' value='moderate' > Moderate</label>
-  <label><input type='radio' name='day' value='active' > Active</label>
-  </div>
+  				      <div class="form-group">
+						        <label>Activity level:</label>
+  					        <label ><input type='radio' name='activity' value='sedentary' checked='checked' <?php if($activity == 'sedentary') echo 'CHECKED'?>> Sedentary</label>
+  					        <label ><input type='radio' name='activity' value='moderate' <?php if($activity == 'moderate') echo 'CHECKED'?>> Moderate</label>
+  					        <label><input type='radio' name='activity' value='active' <?php if($activity == 'active') echo 'CHECKED'?>> Active</label>
+  				      </div>
 
-	<div class="form-group">
-	<label><input type='checkbox' name='calories' value='calories' >Include calorie requirement</label>
-  </div>
+					      <div class="form-group">
+						         <label><input type='checkbox' name='calories' value='calories' <?php if($form->isChosen('calories')) echo 'CHECKED' ?>>Include calorie requirement</label>
+  				      </div>
 
-	<button type='submit' class='btn btn-primary btn-sm'>Calculate</button>
+  				      <div class="form-group">
+						        <button type='submit' class='btn btn-primary btn-small'>Calculate</button>
+  				      </div>
+				    </fieldset>
+  		  </form>
 
-  </fieldset>
+				<?php if($form->isSubmitted()): ?>
+            <?php if($errors): ?>
+		            <div class='alert alert-danger'>
+			              <ul>
+				                <?php foreach($errors as $error): ?>
+					                  <li><?=$error?></li>
+				                <?php endforeach; ?>
+			              </ul>
+		            </div>
 
+    		    <?php else: ?>
+		            <div class='alert alert-success'>Your BMI is <?=$bmi?></div>
+		            <?php if($calChecked): ?>
+			               <div class='alert alert-success'>Your required calorie intake is <?=$calories?></div>
+		            <?php endif; ?>
+            <?php endif; ?>
+        <?php endif; ?>
 
-    </form>
-</body>
-
+		</body>
 </html>
