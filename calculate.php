3173555d3677c52746de9b014a411cb4a6a367cf
Varun Suresh <varunsureshkumar@g.harvard.edu>
2017-02-21 20:39:02 +0530

    Logic file for BMI Calculator

---
+++ calculate.php
@@ -0,0 +1,40 @@
+<?php
+
+require('Bmi.php');
+require('Form.php');
+require('tools.php');
+
+$bmiObject= new Bmi();
+$form= new DWA\Form($_GET);
+
+$calChecked=false; #calChecked keeps track of whether the check
+$info=true;
+$submitted=false;
+
+$weight = $form->sanitize($form->get('weight',$default=1));
+$height = $form->sanitize($form->get('height',$default=1));
+$age = $form->sanitize($form->get('age',$default=0));
+$gender = $form->get('gender',$default='male');
+$activity = $form->get('activity',$default='sedentary');
+$calChecked= $form->isChosen('calories');
+
+if ($form->isSubmitted()){
+    $submitted=true;
+
+    $errors = $form->validate(
+    [
+        'weight'=>'required|numeric',
+        'height'=>'required|numeric',
+        'age'=>'required|numeric',
+    ]
+    );
+
+    if($errors)
+        $info=false;
+
+    $bmi=$bmiObject->bmiCal($weight,$height);
+
+    if($calChecked){
+        $calories=$bmiObject->caloriesCal($weight, $height,$gender,$age,$activity);
+    }
+}
